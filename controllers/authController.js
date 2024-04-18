f42019d6ce23b269aaed3a46e4630fd32b4bf6b3
Sammy <samakan061@gmail.com>
2024-04-18 13:44:45 +0300

    Sending html email implemented

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,8 +4,9 @@ const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const AppError = require('./../utils/appError');
 const catchAsync = require('./../utils/catchAsync');
-const sendEmail = require('./../utils/email');
+const Email = require('./../utils/email');
 const bcrypt = require('bcryptjs');
+const { request } = require('http');
 
 /**
  *
@@ -62,6 +63,8 @@ exports.signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
   });
+  const url = `${req.protocol}://${req.get('host')}/me`;
+  await new Email(newUser, url).sendWelcome();
 
   createSendToken(newUser, 201, res);
 });
@@ -204,26 +207,11 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 
   const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetpassword/${resetToken}`;
 
-  const message = `Forgot your password? Submit a request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email`;
-
-  try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token valid for 10 mins',
-      message: message,
-    });
-
-    res.status(200).json({
-      status: 'success',
-      message: 'Token sent to email',
-    });
-  } catch (err) {
-    user.passwordResetToken = undefined;
-    user.passwordResetExpires = undefined;
-    await user.save({ validateBeforeSave: false });
-
-    return next(new AppError('There was an error sending email', 500));
-  }
+  await new Email(user, resetURL).sendResetToken();
+  res.status(200).json({
+    status: 'success',
+    message: 'recovery link has been sent to user email',
+  });
 });
 
 /**
--- utils/email.js
+++ utils/email.js
@@ -1,28 +1,63 @@
 const nodemailer = require('nodemailer');
-const catchAsync = require('./catchAsync');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = catchAsync(async (options) => {
-  // 1 Create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Akan Sam <${process.env.EMAIL_FROM}>`;
+  }
 
-  // 2 Define the email options
-  const mailOptions = {
-    from: 'Akan Sam <contact@sam.io>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // Sendgrid
+      return 1;
+    }
 
-  // 3 Actually send the email with nodemailer
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
 
-  await transporter.sendMail(mailOptions);
-});
+  async send(template, subject) {
+    // 1 Render HTML based on pug template
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject: subject,
+      },
+    );
 
-module.exports = sendEmail;
+    // views\emails\welcome.pug  C:\Users\zxcv2\OneDrive\Рабочий стол\Devs\MyToursAppNodeJS\views\emails\welcome.pug
+
+    // Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject: subject,
+      html: html,
+      text: htmlToText.convert(html),
+    };
+
+    // Create a transport and send email
+
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the tour App');
+  }
+
+  async sendResetToken() {
+    await this.send('passwordReset', 'Reset your password');
+  }
+};
